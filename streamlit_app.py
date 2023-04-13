0a6e1399e8bb4ddd2e7c1c6c4779434434002310
Kimberly Hahn <130709382+Kimberly-YuvoHealthCare@users.noreply.github.com>
2023-04-13 17:45:51 -0500

    Update streamlit_app.py
    typo correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlist.header('Breakfast Menu')
-streamlist.text('Omega 3 Blueberry Oatmeal')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free Range Egg')
